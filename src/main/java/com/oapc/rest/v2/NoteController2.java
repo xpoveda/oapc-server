0ec69a1f3aefdf062cc28381a339ea2697e97480
xpoveda <xpoveda@LAPTOP-DHBDH0JC>
2018-02-26 11:44:45 +0100

    modificacion

---
+++ src/main/java/com/oapc/rest/v2/NoteController2.java
@@ -0,0 +1,64 @@
+package com.oapc.rest.v2;
+
+import com.oapc.model.Note;
+import com.oapc.repo.NoteRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v2")
+public class NoteController2 {
+
+    @Autowired
+    NoteRepository noteRepository;
+
+    @GetMapping("/notes")
+    public List<Note> getAllNotes() {
+        return noteRepository.findAll();
+    }
+
+    @GetMapping("/notes/{id}")
+    public ResponseEntity<Note> getNoteById(@PathVariable(value = "id") Long noteId) {
+        Note note = noteRepository.findOne(noteId);
+        if(note == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok().body(note);
+    }
+
+    @PostMapping("/notes")
+    public Note createNote(@Valid @RequestBody Note note) {
+        return noteRepository.save(note);
+    }
+
+    @PutMapping("/notes/{id}")
+    public ResponseEntity<Note> updateNote(@PathVariable(value = "id") Long noteId,
+                                           @Valid @RequestBody Note noteDetails) {
+    	
+        Note note = noteRepository.findOne(noteId);
+        if(note == null) {
+            return ResponseEntity.notFound().build();
+        }
+        note.setTitle(noteDetails.getTitle());
+        note.setContent(noteDetails.getContent());
+
+        Note updatedNote = noteRepository.save(note);
+        return ResponseEntity.ok(updatedNote);
+    }
+
+    @DeleteMapping("/notes/{id}")
+    public ResponseEntity<Note> deleteNote(@PathVariable(value = "id") Long noteId) {
+        Note note = noteRepository.findOne(noteId);
+        if(note == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        noteRepository.delete(note);
+        return ResponseEntity.ok().build();
+    }
+}
